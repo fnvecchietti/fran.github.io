504b53764bc64a67b635b9606e3a64ecfdc506b2
Francisco <francisco@example.com>
2024-07-08 19:22:48 -0300

    solving importing issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fran.github.io',
+  site: 'https://github.com/fnvecchietti/fran.github.io',
   integrations: [tailwind()]
 });
