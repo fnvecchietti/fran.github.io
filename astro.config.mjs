48cc7d9ec2ae7d8ca93b06bf752f4469ee405616
Francisco <francisco@example.com>
2024-07-08 19:45:20 -0300

    fixing

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://fnvecchietti.github.io/portfolio',
+  site: 'https://fnvecchietti.github.io',
+  base: '/portfolio',
   integrations: [tailwind()]
 });
